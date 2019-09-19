7bd96c5c7b7879d930cd43dc3bf9f97ace9019f4
Lee <13500502420@163.com>
2019-09-19 13:29:34 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/text")
+    @ResponseBody
+    public String say(){
+        return "springboot 初体验";
+    }
+
+    @RequestMapping("/thymeleaf")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
